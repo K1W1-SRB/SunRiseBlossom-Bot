e9183f4d7b7a9a7b0c5270a5e50e101ef4d6c507
K1W1-SRB <sebastiansomogyi@gmail.com>
2022-07-31 15:59:58 +0100

    status changer

--- features/status-changer.ts
+++ features/status-changer.ts
@@ -1,29 +1,28 @@
-import { Client } from 'discord.js'
+import { Client } from "discord.js";
 
 export default (client: Client) => {
-    const statusOptions = [ 'hello', 'NIgger','balls']
-    let counter = 0
+  const statusOptions = ["hello", "Iam", "Bot"];
+  let counter = 0;
 
-    const updateStatus = () => {
-        client.user?.setPresence({
-            status: 'online',
-            activities: [
-                {
-                name: statusOptions[counter]
-                }
-            ]
-        })
-        if(++counter >= statusOptions.length) {
-            counter = 0
-        }
-
-        setTimeout(updateStatus, 1000 * 5)
+  const updateStatus = () => {
+    client.user?.setPresence({
+      status: "online",
+      activities: [
+        {
+          name: statusOptions[counter],
+        },
+      ],
+    });
+    if (++counter >= statusOptions.length) {
+      counter = 0;
     }
-    updateStatus()
-}
+
+    setTimeout(updateStatus, 1000 * 5);
+  };
+  updateStatus();
+};
 
 export const config = {
-    dbName: 'STATUS_CHANGER',
-    displayName: 'Status Changer'
-}
-
+  dbName: "STATUS_CHANGER",
+  displayName: "Status Changer",
+};
